5beb38bb4264c9db0db4f3a7a401809f18451775
Thanakorn Piroonsith <thanakorn@extentsoft.com>
2017-07-28 14:27:42 +0700

    Developed 3rd TDD Testbed which is now failed after binding promise.then

--- tests/tdd/test1_index_route.js
+++ tests/tdd/test1_index_route.js
@@ -5,18 +5,44 @@ var sinonChai = require('sinon-chai');
 chai.use(sinonChai);
 chai.should();
 
-// Object to mock the articles model
-var itemMock = {};
-var test1 = require('../../routes/test1.js')({Item: itemMock});
-
-// Snip - Variable and function declarations removed for brevity
-
 describe('Test1 Index Route', function(){
+  var itemMock;     // Object to mock the item model
+  var promiseMock;  // Mock for the promise returned by item for index
+  var response;     //Object to mock the response
+  var test1;        //Test1 Route
+
+  function requireTest1(models){
+    return require('../../routes/test1.js')(models);
+  }
+  function callTest1IndexRoute(){
+    test1.index({}, response);
+  }
+
+  beforeEach(function(){
+    response = {};
+    promiseMock = {};
+    itemMock = {};
+
+    itemMock.itemForIndex = sinon.stub()
+      .returns(promiseMock);
+
+    promiseMock.then = sinon.spy();
+
+    test1 = requireTest1({Item: itemMock});
+  });
+
   it('calls Item.itemForIndex', function(){
-    itemMock.itemForIndex = sinon.spy();
-
-    test1.index({},{});
-
+    callTest1IndexRoute();
     itemMock.itemForIndex.should.have.been.calledOnce;
+  });
+
+  it('passes resolve and reject functions to itemForIndex', function(){
+    callTest1IndexRoute();
+    var args = promiseMock.then.getCall(0).args;
+
+    args.length.should.equal(2);
+
+    args[0].should.be.a.function;
+    args[1].should.be.a.function;
   });
 });
