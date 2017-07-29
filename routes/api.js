f3fd4dd8d2ada31384afa298fe47fd8459f1c303
Thanakorn Piroonsith <thanakorn@extentsoft.com>
2017-07-30 06:51:15 +0700

    Added DB(mongoDB) functionalities to API

--- routes/api.js
+++ routes/api.js
@@ -3,134 +3,152 @@ var router = express.Router();
 var loki = require('lokijs'),
   db = new loki('stylecooker.json');
 
-var items = db.addCollection('items');
-var orders = db.addCollection('orders');
-router.get('/test/item', function(req,res){
+var MongoObject = require('mongodb');
+var mongo = MongoObject.MongoClient;
+var ObjectId = MongoObject.ObjectID;
 
-  res.send('Done mocking');
-});
-router.get('/test/order', function(req,res){
+var db;
+mongo.connect('mongodb://scadmin:123456@ds159892.mlab.com:59892/stylecooker',function(err,database){
+  if(err) console.log('Database link down');
 
+  console.log('Connect Success');
+  db = database;
 });
+
 
 /*  **************************
     ****** API for Item ******
 */
 /* [GET /api/item] - Get all items */
 router.get('/item', function(req,res){
-  res.send(items.data);
+  var itemObj = db.collection('items');
+
+  itemObj.find({}).toArray(function(err, docs) {
+    console.log(docs);
+    res.send({items: docs});
+  });
 });
 
 /* [GET /api/item/id] - Get specific items */
 router.get('/item/:id', function(req,res){
-  res.send(items.find({sku : req.params.id}));
+  var itemObj = db.collection('items');
+
+  itemObj.find({sku:req.params.id}).toArray(function(err, docs) {
+    console.log(docs);
+    res.send({items: docs});
+  });
+
 });
 
 /* [POST /api/item] - Create a new item */
 router.post('/item', function(req,res){
-  console.log(req.body);
-  items.insert(req.body);
-  /*items.insert({
-    sku: 'req.',
-    name: 'TESTBB23',
-    color: '#FF00AE',
-    amount_xs: 2,
-    amount_s: 1,
-    amount_m: 5,
-    amount_l: 3,
-    amount_xl: 1,
-    updated_at: Date.now()
-  });*/
-  res.send(200);
+  db.collection('items').save(req.body, function(err,result){
+    if(err) return console.log(err);
+
+    res.send(200);
+  });
 });
-
-/*
-[
-    {
-        "sku": "10231412",
-        "name": "TESTBB23",
-        "color": "#FF00AE",
-        "amount_xs": 2,
-        "amount_s": 1,
-        "amount_m": 5,
-        "amount_l": 3,
-        "amount_xl": 1,
-        "updated_at": 1501360164319,
-        "meta": {
-            "revision": 0,
-            "created": 1501360164319,
-            "version": 0
-        },
-        "$loki": 1
-    },
-    {
-        "sku": "10231412",
-        "name": "TESTBB23",
-        "color": "#FF00AE",
-        "amount_xs": 2,
-        "amount_s": 1,
-        "amount_m": 5,
-        "amount_l": 3,
-        "amount_xl": 1,
-        "updated_at": 1501360166967,
-        "meta": {
-            "revision": 0,
-            "created": 1501360166967,
-            "version": 0
-        },
-        "$loki": 2
-    }
-]
-*/
-
 
 /* [PUT /api/item/id] - Update specific item */
 router.put('/item/:id', function(req,res){
-  var updating_item = items.find({sku : req.params.id});
-  updating_item.name = req.body.name;
-  updating_item.amount_xs = req.body.amount_xs;
-  updating_item.amount_s = req.body.amount_s;
-  updating_item.amount_m = req.body.amount_m;
-  updating_item.amount_l = req.body.amount_l;
-  updating_item.amount_xl = req.body.amount_xl;
-  items.update(updating_item);
-  res.send(200);
+  db.collection('items').updateOne(
+    {
+      sku:req.params.id
+    },
+    {
+      $set:{
+        name: req.body.name,
+        amount_xs: req.body.amount_xs,
+        amount_s: req.body.amount_s,
+        amount_m: req.body.amount_m,
+        amount_l: req.body.amount_l,
+        amount_xl: req.body.amount_xl
+      }
+    }, function(err,result){
+      if(err) return console.log(err);
+
+      console.log('Update Success');
+      res.send(200);
+    }
+  );
 });
 
 /* [DELETE /api/item/id] - Delete specific item */
 router.delete('/item/:id', function(req,res){
-  res.send('[DELETE /api/item/id]');
+  var itemObj = db.collection('items');
+
+  itemObj.deleteOne({sku:req.params.id}, function(err, docs) {
+    console.log(docs);
+    res.send(200);
+  });
 });
 
 
 /*  **************************
     ****** API for Order ******
 */
-/* [GET /api/order] - Get all items */
+/* [GET /api/order] - Get all orders */
 router.get('/order', function(req,res){
-  res.send('[GET /api/order]');
+  var orderObj = db.collection('orders');
+
+  orderObj.find({}).toArray(function(err, docs) {
+    console.log(docs);
+    res.send({orders: docs});
+  });
 });
 
-/* [GET /api/order/id] - Get specific items */
+/* [GET /api/order/id] - Get specific order */
 router.get('/order/:id', function(req,res){
-  res.send('[GET /api/order/id]');
+  var orderObj = db.collection('orders');
+                     
+  orderObj.find({_id:ObjectId(req.params.id)}).toArray(function(err, docs) {
+    console.log(docs);
+    res.send({orders: docs});
+  });
+
 });
 
-/* [POST /api/order] - Create a new item */
+/* [POST /api/order] - Create a new order */
 router.post('/order', function(req,res){
-  res.send('[POST /api/order]');
+  console.log(req.body);
+  db.collection('orders').save(req.body, function(err,result){
+    if(err) return console.log(err);
+
+    res.send(200);
+  });
 });
 
-/* [PUT /api/order/id] - Update specific item */
+/* [PUT /api/order/id] - Update specific order */
 router.put('/order/:id', function(req,res){
-  res.send('[PUT /api/order/id]');
+  db.collection('orders').updateOne(
+    {
+      _id:ObjectId(req.params.id)
+    },
+    {
+      $set:{
+        item_id: req.body.item_id,
+        mobile: req.body.mobile,
+        size: req.body.size,
+        amount: req.body.amount,
+        total: req.body.total
+      }
+    }, function(err,result){
+      if(err) return console.log(err);
+
+      console.log('Update Success');
+      res.send(200);
+    }
+  );
 });
 
-/* [DELETE /api/item/id] - Delete specific item */
+/* [DELETE /api/order/id] - Delete specific order */
 router.delete('/order/:id', function(req,res){
-  res.send('[DELETE /api/order/id]');
+  var orderObj = db.collection('orders');
+
+  orderObj.deleteOne({_id:ObjectId(req.params.id)}, function(err, docs) {
+    console.log(docs);
+    res.send(200);
+  });
 });
-
-
 
 module.exports = router;
