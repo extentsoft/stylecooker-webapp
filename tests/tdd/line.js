c0369fdae8997d39ddd77db5f405610ffc3c339f
Thanakorn Piroonsith <thanakorn@extentsoft.com>
2017-07-29 12:42:19 +0700

    Added LINE TDD Testsuit

--- tests/tdd/line.js
+++ tests/tdd/line.js
@@ -2,4 +2,33 @@ var nock = require('nock');
 var chai = require('chai');
 var expect = chai.expect;
 
-//https://www.codementor.io/nodejs/tutorial/unit-testing-tdd-node-js-nockjs-part-2 
+ describe("LINE Integration", function(){
+  /*
+      As LINE,
+      it needs to send us the information via /webhook
+      so that conversation made by LINE user could be delivered to us
+  */
+  it('should get accessible via /webhook', function(done){
+
+  });
+
+  /*
+      As system,
+      it needs source information for extracting userID
+      so that it can reply to receipeint correctly
+  */
+  it('should get source information', function(done){
+
+  });
+
+  /*
+      As system,
+      it needs to know what is the initial context from user
+      so that it could select the conversation correctly
+  */
+  it('should get message information', function(done){
+
+  });
+
+ });
+//https://www.codementor.io/nodejs/tutorial/unit-testing-tdd-node-js-nockjs-part-2
